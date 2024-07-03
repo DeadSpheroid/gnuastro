ee06362285054db4e0489bf501d23d11ce5659d5
Warren Jacinto <warrenjacinto@gmail.com>
2024-07-04 03:27:58 +0530

    Scripts: new script cl-clean.sh and minor changes to other scripts
    cl-clean.sh: new script removing any images generated by other
    scripts(test-cl-convolve.sh and cl-compare.sh).
    
    cl-compare.sh: formatting changes.
    
    test-cl-convolve.sh: allow user to choose astconvolve binary and
    size of input image for the test.

--- cl-compare.sh
+++ cl-compare.sh
@@ -1,20 +1,39 @@
 if [ -e cl_cpu_conv.fits ] && [ -e cl_gpu_conv.fits ]; then
-    echo -e "----------------------\033[1;33mOpenCL CPU versus OpenCL GPU\033[0m----------------------"
-    astarithmetic --output=diff_cl_cpu_vs_cl_gpu.fits --hdu=1 --hdu=1 cl_cpu_conv.fits cl_gpu_conv.fits - > /dev/null
+    echo -e "----------------------\033[1;33mOpenCL CPU versus\
+ OpenCL GPU\033[0m----------------------"
+
+    astarithmetic --output=diff_cl_cpu_vs_cl_gpu.fits --hdu=1 --hdu=1 \
+                    cl_cpu_conv.fits cl_gpu_conv.fits - > /dev/null
+
     aststatistics diff_cl_cpu_vs_cl_gpu.fits
+
     echo
 fi
+
+
 
 if [ -e pthread_cpu_conv.fits ] && [ -e cl_gpu_conv.fits ]; then
-    echo -e "----------------------\033[1;33mPthread CPU versus OpenCL GPU\033[0m----------------------"
-    astarithmetic --output=diff_pthread_cpu_vs_cl_gpu.fits --hdu=1 --hdu=1 pthread_cpu_conv.fits cl_gpu_conv.fits - > /dev/null
+    echo -e "----------------------\033[1;33mPthread CPU versus\
+ OpenCL GPU\033[0m----------------------"
+
+    astarithmetic --output=diff_pthread_cpu_vs_cl_gpu.fits --hdu=1 \
+                  --hdu=1 pthread_cpu_conv.fits cl_gpu_conv.fits - > /dev/null
+
     aststatistics diff_pthread_cpu_vs_cl_gpu.fits
+
     echo
 fi
 
+
+
 if [ -e pthread_cpu_conv.fits ] && [ -e cl_cpu_conv.fits ]; then
-    echo -e "----------------------\033[1;33mPthread CPU versus OpenCL CPU\033[0m----------------------"
-    astarithmetic --output=diff_pthread_cpu_vs_cl_cpu.fits --hdu=1 --hdu=1 pthread_cpu_conv.fits cl_cpu_conv.fits - > /dev/null
+    echo -e "----------------------\033[1;33mPthread CPU versus\
+ OpenCL CPU\033[0m----------------------"
+
+    astarithmetic --output=diff_pthread_cpu_vs_cl_cpu.fits --hdu=1 \
+                  --hdu=1 pthread_cpu_conv.fits cl_cpu_conv.fits - > /dev/null
+
     aststatistics diff_pthread_cpu_vs_cl_cpu.fits
+
     echo
 fi
